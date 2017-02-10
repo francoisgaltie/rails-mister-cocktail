684e978b1821f4b50ca435e09a7b7fe62854d008
Francois Galtie <francois.galtie@gmail.com>
2017-02-10 09:40:54 +0100

    Added figaro - Protect my secret data in config/application.yml

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,14 +8,18 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktails/show'
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@dose.cocktail)
+    cocktail = @dose.cocktail
+    redirect_to cocktail_path(cocktail)
   end
 
   private
